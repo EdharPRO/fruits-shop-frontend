4873848fa77ce88091ce39b5b61e7c16e7490e3c
Edhar Prokudin <prokudin.ed@gmail.com>
2022-11-17 09:26:25 +0200

    add sign in modal react-hook-form

--- src/components/Header/SignIn/SignIn.jsx
+++ src/components/Header/SignIn/SignIn.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import {
   Avatar,
   Box,
@@ -22,6 +23,21 @@ const Transition = forwardRef(function Transition(props, ref) {
 
 export const SignIn = () => {
   const [ open, setOpen ] = useState(false);
+  const {
+    register,
+    formState: {
+      errors
+    },
+    handleSubmit,
+    reset
+  } = useForm({
+    mode: "onBlur"
+  });
+
+  const signIn = (data) => {
+    console.log(data);
+    reset();
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -33,8 +49,8 @@ export const SignIn = () => {
 
   return (
     <>
-      <IconButton color="inherit">
-        <AccountCircleIcon onClick={handleClickOpen} />
+      <IconButton onClick={handleClickOpen} color="inherit">
+        <AccountCircleIcon />
       </IconButton>
 
       <Dialog
@@ -47,7 +63,8 @@ export const SignIn = () => {
             spacing={1}
             sx={{
               justifyContent: 'center',
-              alignItems: 'center'
+              alignItems: 'center',
+              textAlign: 'center'
             }}
           >
             <Avatar sx={{ bgcolor: 'tomato' }} >
@@ -56,14 +73,51 @@ export const SignIn = () => {
 
             <Typography variant="subtitle1">Sign In</Typography>
 
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-              <TextField  sx={{ mb: 1 }} label="E-mail"/>
-              <TextField type="password" label="Password" />
-            </Box>
+            <form onSubmit={handleSubmit(signIn)}>
+              <TextField
+                  {...register("Email", {
+                    required: "Write your e-mail, please",
+                    minLength: {
+                      message: "Minimum 5 letters",
+                      value: 5
+                    }
+                  })}
 
-            <FormControlLabel control={<Checkbox />} label="Remember Me" />
+                  sx={{ mb: 2 }}
+                  type="text"
+                  label="E-mail"
+              />
+              <Box>
+                {errors?.Email && <Typography variant="subtitle2">{errors?.Email?.message }</Typography>}
+              </Box>
 
-            <Button variant="contained">Sign In</Button>
+              <br />
+              <TextField
+                  {...register('Password', {
+                    required: "Write your password, please",
+                    minLength: {
+                      message: "Minimum 5 letters",
+                      value: 5
+                    }
+                  })}
+
+                  sx={{ mb: 2 }}
+                  type="password"
+                  label="Password"
+              />
+              <Box>
+                {errors?.Password && <Typography variant="subtitle2">{errors?.Password?.message }</Typography>}
+              </Box>
+
+              <br />
+              <FormControlLabel
+                  sx={{ mb: 1 }}
+                  control={<Checkbox />}
+                  label="Remember Me"
+              />
+              <br />
+              <Button type="submit" variant="contained">Sign In</Button>
+            </form>
           </Stack>
         </Paper>
       </Dialog>
