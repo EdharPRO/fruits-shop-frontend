775a9da0c424b1f84c71359c4a1c1be2991dff0e
Edhar Prokudin <prokudin.ed@gmail.com>
2022-11-22 21:16:21 +0200

    up all page Checkout

--- src/components/Checkout/Review/Review.jsx
+++ src/components/Checkout/Review/Review.jsx
@@ -1,67 +1,44 @@
-import { Box, Button, List, ListItem, ListItemText, Container, Grid, Paper, Stack, Typography } from '@mui/material';
-import { useContext } from 'react';
-import { PurchasesContext } from '../../../modules/purchases';
+import { Button, List, ListItem, ListItemText, Stack, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-const products = [
-  {
-    name: 'Product 1',
-    desc: 'A nice thing',
-    price: '$9.99'
-  },
-  {
-    name: 'Product 2',
-    desc: 'Another thing',
-    price: '$3.45'
-  },
-  {
-    name: 'Product 3',
-    desc: 'Something else',
-    price: '$6.51'
-  },
-  {
-    name: 'Product 4',
-    desc: 'Best thing of all',
-    price: '$14.11'
-  },
-  { name: 'Shipping', desc: '', price: 'Free' }
-];
+export const Review = ({ order }) => {
+  const totalPrice = order.purchases.reduce((sum, { product, count }) => {
+    return sum + product.price * count;
+  }, 0);
 
-export const Review = () => {
-
-  const { purchases } = useContext(PurchasesContext);
 
   return (
-    <Container
-      maxWidth='md'
+    <Stack
+      spacing={1}
       sx={{
-        display: 'flex',
+        justifyContent: 'center',
         alignItems: 'center',
-        justifyContent:'center',
-        textAlign:'center',
-        mt: 3
+        textAlign: 'center'
       }}
     >
-      <Paper elevation={0} sx={{ padding: 2, maxWidth: 500 }}>
-        <Stack
-          spacing={1}
-          sx={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            textAlign: 'center'
-          }}
-        >
-          <Typography variant="subtitle1">Order summary</Typography>
+      <Typography variant="subtitle1">Order summary</Typography>
 
-          <List disablePadding>
-            {purchases.map((purchase) => (
-              <ListItem key={purchase.id} sx={{ py: 1, px: 0, minWidth: 300 }}>
-                <ListItemText primary={purchase.product.name} secondary={purchase.product.price} />
-                <Typography variant="body2">${purchase.product.price}.00</Typography>
-              </ListItem>
-            ))}
-          </List>
-        </Stack>
-      </Paper>
-    </Container>
+      <List disablePadding>
+        {order.purchases.map((purchase) => (
+          <ListItem key={purchase.id} sx={{ py: 1, px: 0, minWidth: 300 }}>
+            <ListItemText primary={purchase.product.name} secondary={purchase.product.price} />
+            <Typography variant="body2">${purchase.product.price}.00</Typography>
+          </ListItem>
+        ))}
+      </List>
+
+      <Typography>${totalPrice}.00</Typography>
+
+      <Stack direction="row" justifyContent="flex-end" spacing={1}>
+        <Button
+          component={Link}
+          to="/"
+          sx={{ mt: 2 }}
+          variant="contained"
+        >
+          Go to main
+        </Button>
+      </Stack>
+    </Stack>
   );
 };
